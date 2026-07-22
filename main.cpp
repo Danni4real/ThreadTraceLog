71922eadb960b402ebc414ba667c122279380e7a
Dan Ni <61080530+Danni4real@users.noreply.github.com>
2026-07-22 15:27:00 +0800

    Add TRACE macro usage example

--- main.cpp
+++ main.cpp
@@ -1,55 +1,46 @@
 #include <thread>
-#include <unistd.h>
 
 #include "ThreadTraceLog.h"
 
-void func_3(int i, bool b, float f, char c, std::string s) {
-    log_call(i,b,f,c,s);
-    log_debug("debug message: %d",333);
+void func_3 TRACE(int,i, bool,b, float,f, char,c, const std::string&,s) // Notice: remove first left brace !!!
 
     {
-        log_scope("scope_a");
+        log_scope("scope");
+
+        log_debug("%s", "debug");
+        log_info("%s", "info");
+        log_warn("%s", "warn");
+        log_error("%s", "error");
     }
 }
 
-void func_2(int x) {
-    log_call_format("x=%d",x);
-    log_info("222");
+void func_2(int i) {
+    log_call_format("i=%d", i);
 
-    func_3(100, false, 3.14, '(', "");
+    func_3(3, false, 3.14, 'a', "bar");
 }
 
-void func_1() {
-    log_call();
-    ThreadTraceLog::log_info("111");
+void func_1(const std::string& s) {
+    log_call(s);
 
-    func_2(20);
-}
-
-void func_0() {
-    log_call();
-    log_debug("000+");
-
-    func_1();
-    log_warn("000-");
+    func_2(2);
 }
 
 int main() {
     log_call();
 
-    std::thread([] {
+    std::thread t1([] {
         log_set_thread_name("thread_1");
-        func_0();
-    }).detach();
+        func_1("fool");
+    });
 
-    std::thread([] {
+    std::thread t2([] {
         log_set_thread_name("thread_2");
-        func_0();
-    }).detach();
+        func_1("fool");
+    });
 
-
-
-    sleep(1);
+    t1.join();
+    t2.join();
 
     return 0;
 }
